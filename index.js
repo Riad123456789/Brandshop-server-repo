a9aa1a56526e69eb19d6445cda3adc8d834c9c56
Riad123456789 <riad80717@gmail.com>
2023-10-20 14:49:03 +0600

    new collection make are done

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ async function run() {
         const BrandCollection = client.db("BrandNameDB").collection("BrandName");
 
 
+        const addCollection = client.db("AddProduct").collection("addProduct");
+
+
         app.get("/products", async (req, res) => {
 
             const cursor = ProductsCollection.find();
@@ -45,17 +48,32 @@ async function run() {
 
         })
 
+        app.get("/product/:name", async (req, res) => {
+            const name = req.params.name;
+            const query = { name: name };
+            const result = await ProductsCollection.findOne(query);
+
+            res.send(result);
+        });
+
+
+
+        app.get("/productss/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await ProductsCollection.findOne(query);
+
+            res.send(result);
+        });
+
 
         app.get("/products/:brandname", async (req, res) => {
             const brandname = req.params.brandname;
-            console.log(brandname)
             const query = { brandname: brandname };
             const cursor = ProductsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
-
-
 
 
 
@@ -67,6 +85,24 @@ async function run() {
 
         })
 
+
+        app.get("/updateproducts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await ProductsCollection.findOne(query);
+
+            res.send(result);
+        });
+
+
+
+        app.get("/cartproduct", async (req, res) => {
+            const cursor = addCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
         app.post('/products', async (req, res) => {
 
             const Products = req.body;
@@ -75,6 +111,48 @@ async function run() {
         })
 
 
+
+
+        app.post("/addproduct", async (req, res) => {
+
+            const product = req.body;
+            const result = await addCollection.insertOne(product);
+
+            res.send(result)
+        })
+
+
+
+        app.put('/updateproducts/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const Products = req.body;
+            const options = { upsert: true };
+            const UpdateProducts = {
+                $set: {
+                    brandname: Products.brandname,
+                    type: Products.type,
+                    price: Products.price,
+                    shortdescription: Products.shortdescription,
+                    rating: Products.rating,
+                    photo: Products.photo,
+                }
+            }
+
+            const result = await ProductsCollection.updateOne(filter, UpdateProducts, options)
+            console.log(result)
+            res.send(result)
+        })
+
+        app.delete("/deletedprodutc/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await addCollection.deleteOne(query);
+            console.log(result)
+            res.send(result);
+
+        })
 
 
         await client.db("admin").command({ ping: 1 });
